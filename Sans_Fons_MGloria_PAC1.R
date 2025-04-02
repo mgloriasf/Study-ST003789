65842f1eb5ca1ac7ccc6a2c27cd18c9449ce13a2
mgloriasf <msansfo@uoc.edu>
2025-04-02 22:08:59 +0200

    Add files via upload

--- Sans_Fons_MGloria_PAC1.R
+++ Sans_Fons_MGloria_PAC1.R
@@ -7,12 +7,14 @@ library(SEtools)
 #S'obté l'objecte SummarizedExperiment a partir del paquet "metabolomicsWorkbenchR". 
 #Permet obtenir directament aquest objecte però també podria crear-se a partir de les
 #dades.
-se=do_query(
+
+se= do_query(
   context='study',
   input_item="study_id",
   input_value="ST003789",
   output_item="SummarizedExperiment"
 )
+
 
 # Existeixen dos "summarizedExperiment" associats a la referència ST003789. 
 #Corresponen a dos objectes que contenen dades de metaboits obtinguts per detecció de 
@@ -33,6 +35,13 @@ dim(m1)
 colData(m1)
 dplyr::glimpse(m1)
 
+#Distribució de les dades
+
+BiocManager::install("PRONE")
+library(PRONE)
+plot_condition_overview(normalized, condition="Diet")
+plot_condition_overview(normalized, condition="Treatment")
+
 #Anàlisis de les dades
 
 BiocManager::install("POMA",force=TRUE)
@@ -42,26 +51,26 @@ library(patchwork)
 library(magrittr)
 library(tidyverse)
 
+#Primerament s'han d'eliminar les mostres que contenen indeterminats o 0.
 
-is.na(se)
-
-
-#treure valors na
-
-class(m1)
-dim(m1)
 imputed<-PomaImpute(m1,method="knn", zeros_as_na=TRUE, remove_na=TRUE,cutoff=20)
 imputed
+
+#Normalització de les dades
+
 normalized<-PomaNorm(imputed,method="log_pareto")
 normalized
-PomaBoxplots(normalized,x="samples")+
-  ggtitle("normalized")+
-  theme(legend.position="none")
+
+#Boxplot i densitat de les dades normalitzades i no normalitzades
+
+PomaBoxplots(normalized,x="samples")
 PomaBoxplots(m1,x="samples")
 PomaDensity(m1,x="samples")
+PomaDensity(normalized,x="samples")
+
+#Detecció de outliers
 
 
-PomaOutliers(normalized,do=clean)
 do<-PomaOutliers(normalized,
              method="euclidean",
              type="median",
@@ -70,19 +79,17 @@ do<-PomaOutliers(normalized,
              labels=FALSE
 )
 do$polygon_plot
-PomaOutliers(normalized,
+
+do2<-PomaOutliers(normalized,
                               method="euclidean",
                               type="median",
                               outcome="Treatment",
                               coef=2,
                               labels=FALSE
 )
+do2$polygon_plot
 
-
-PomaOutliers(outlier_results,do="clean")
-
-PomaUnivariate(normalized,method="ttest",covs= Null,error=NULL, paired=FALSE,adjust="fdr")
-
+#Anàlisis estadística: components principals
 
 PomaPCA(
   normalized,
@@ -108,7 +115,7 @@ PomaPCA(
 )
 
 
-
+#Correlació de les dades
 
 poma_cor <- PomaCorr(normalized)
 poma_cor$correlations
@@ -119,5 +126,4 @@ library(PRONE)
 plot_condition_overview(normalized, condition="Diet")
 plot_condition_overview(normalized, condition="Treatment")
 
-plot_NA_heatmap(normalized, color_by=NULL,label_by=NULL, cluster_samples=TRUE,cluster_proteins= TRUE)
 
