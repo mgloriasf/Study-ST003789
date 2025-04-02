24ee7c72b8ce730f2e5117191f29d208ac21594b
mgloriasf <msansfo@uoc.edu>
2025-04-02 16:52:34 +0200

    codi

--- Sans_Fons_MGloria_PAC1.R
+++ Sans_Fons_MGloria_PAC1.R
@@ -22,3 +22,88 @@ m1<-se$AN006228
 m1
 m2<-se$AN006229
 m2
+
+#Observació de les dades
+
+ncol(m1)
+class(m1)
+head(m1)
+tail(m1)
+dim(m1)
+colData(m1)
+dplyr::glimpse(m1)
+
+#Anàlisis de les dades
+
+BiocManager::install("POMA",force=TRUE)
+library(POMA)
+library(ggtext)
+library(patchwork)
+library(magrittr)
+library(tidyverse)
+
+#treure valors na
+class(de)
+dim(se$AN006228)
+imputed<-PomaImpute(m1,method="knn", zeros_as_na=TRUE, remove_na=TRUE,cutoff=20)
+dim(imputed)
+normalized<-PomaNorm(imputed,method="log_pareto")
+normalized
+PomaBoxplots(normalized,x="samples")+
+  ggtitle("normalized")+
+  theme(legend.position="none")
+PomaBoxplots(x,x="samples")
+PomaDensity(x,x="samples")
+
+
+PomaOutliers(normalized,do=clean)
+PomaOutliers(normalized,
+             do="clean",
+             method="euclidean",
+             type="median",
+             outcome="Diet",
+             coef=2,
+             labels=FALSE
+)
+outlier_results<-PomaOutliers(normalized,
+                              method="euclidean",
+                              type="median",
+                              outcome="Treatment",
+                              coef=2,
+                              labels=FALSE
+)
+
+
+PomaOutliers(outlier_results,do="clean")
+PomaUnivariate(normalized,method="ttest",covs=NULL,error=NULL, paired=FALSE,adjust="fdr")
+
+
+PomaPCA(
+  normalized,
+  outcome="Treatment",
+  center=TRUE,
+  scale=TRUE,
+  ncomp=5,
+  labels=FALSE,
+  ellipse=FALSE,
+  load_length=1
+)
+
+PomaPCA(
+  normalized,
+  outcome="Diet",
+  center=TRUE,
+  scale=TRUE,
+  ncomp=5,
+  labels=FALSE,
+  ellipse=FALSE,
+  load_length=1
+)
+
+
+
+
+poma_cor <- PomaCorr(normalized)
+poma_cor$correlations
+poma_cor$corrplot
+
